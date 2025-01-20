62c215556db75bd761b26fe52db43130cb11e2f7
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-20 19:28:19 +0600

    Add files via upload

---
+++ Num2.java
@@ -0,0 +1,17 @@
+public class Num2{
+
+
+public static void main (String[] arrgs)
+
+{   int n1  = 1;
+    int n2  = 1;
+    int n3  = 1;
+    int n4  = 1;
+
+    int sum = n1+n2+n3+n4;
+
+    System.out.print(sum);
+}
+
+
+}
