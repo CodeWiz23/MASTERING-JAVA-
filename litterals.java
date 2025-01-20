3940502fdaa6b867dbc6dc165ac889284f1ff8b5
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-20 21:36:22 +0600

    Add files via upload

---
+++ litterals.java
@@ -0,0 +1,18 @@
+public class litterals {
+
+ public static void main (String[]args)
+ {
+   byte a = 77 ; // -127 t0 127
+   int b = 2;
+   short g = 5;
+   float c = 4.88f;
+   double d = 4.77;
+   char e = 'A';
+   long f = 6666666l;
+   String i = "Murslain";
+   
+   boolean h = true ;
+   System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h+" "+i);
+ }
+
+}
