a2c76421c16f797363ab4d921c93e73f22d1dfca
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-21 19:08:09 +0600

    Add files via upload

---
+++ boolean1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class boolean1{
+
+    public static void main(String[]args)
+    {
+        Scanner s = new Scanner (System.in);
+        System.out.print("Enter the INT number to cheek : ");
+
+
+
+        boolean b = s.hasNextInt();
+
+        System.out.println(b);
+
+
+    }
+
+
+
+}
---
+++ string1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class string1 {
+    public static void main(String[]args)
+    {
+     
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Line :");
+
+        String a =s.nextLine();
+
+        System.out.println(a);
+
+    }
+
+}
