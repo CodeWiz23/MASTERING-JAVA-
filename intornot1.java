af8bb787ad5d463ce86ffb49fcc86992115a0471
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-23 09:27:43 +0600

    Add files via upload

---
+++ intornot1.java
@@ -0,0 +1,32 @@
+/*
+import java.util.Scanner;
+public class intornot1 {
+    public static void main (String[]args)
+    {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the int number :");
+
+        System.out.println( s.hasNextInt());
+
+
+
+    }
+
+}
+*/
+import java.util.Scanner;
+public class intornot1 {
+    public static void main (String[]args)
+    {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the int number :");
+                boolean a = s.hasNextInt();
+        System.out.println(a);
+
+
+
+    }
+
+}
---
+++ oparators.java
@@ -0,0 +1,29 @@
+public class oparators {
+    public static void main(String[] args) {
+
+       // and op (&&)
+    /*    if (4 > 2 && 3 < 1 && 10 > 3) {
+            System.out.println("True");
+        }
+         else {
+            System.out.println("False");
+        }
+    }*/
+
+    // Or operator (||)
+        if (4 > 5 || 3 < 5 || 1 > 10) {
+        System.out.println("Any argument is true");
+    }
+        else {
+        System.out.println("No argument is true ");
+    }
+}
+
+
+
+
+
+
+
+    }
+
