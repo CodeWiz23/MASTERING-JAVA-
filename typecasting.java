8bdcb3c2e7a018ef7622959ee7d3cc68f0e2bbbd
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-24 11:43:28 +0600

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+//forcing to show different results
+
+public class typecasting {
+
+
+    public static void main(String[]args)
+    {
+
+       char grade = 'A';
+        System.out.println((char) (grade+2));// here the result will be 67 but for type casting (grade) it will show C
+        
+
+    }
+}
