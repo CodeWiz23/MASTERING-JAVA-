7a49e5363c6e293502d19fd2ce0625421e3241ac
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-24 11:01:33 +0600

    Add files via upload

---
+++ increament_dec.java
@@ -0,0 +1,20 @@
+public class increament_dec {
+
+
+    public static void main(String[]args)
+    {
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a++); // Here the ans is 10 for its assign the 10 on (a) 1s
+        System.out.println(a); // Here the ans will be 11 because its increasing 1 + on previous assign (a)
+        System.out.println(++b); // Here the ans will be 21 because its increasing 1 + on (b) 1st
+        System.out.println(b); // Here the ans will be 21 because it's been previously assigned
+
+
+
+    }
+
+
+
+}
