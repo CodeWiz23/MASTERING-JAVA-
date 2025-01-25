b5cb61f470e6400454f3daa637cdd0933a80e657
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-25 10:03:17 +0600

    Add files via upload

---
+++ prac.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class prac {
+
+    public static void main(String[]args)
+    {
+       Scanner sc  = new Scanner(System.in);
+        System.out.println("How many times you want to print Hello World ?");
+       int a = sc.nextInt();
+
+
+           for (int i =0;i<a;i++)
+
+           {
+               System.out.println("Hello World");
+
+           }
+
+
+
+
+
+
+
+
+    }
+}
