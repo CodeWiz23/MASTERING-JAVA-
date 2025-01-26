d5742bccc10345e287010c9babd9e26c70065844
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-26 22:22:20 +0600

    Add files via upload

---
+++ UseseOfFunction.java
@@ -0,0 +1,14 @@
+public class UseseOfFunction {
+    int sum (int a,int b)
+    {
+       return a + b;
+
+    }
+
+    public static void main (String[]args){
+
+        UseseOfFunction obj =new UseseOfFunction();
+        System.out.println("The ans is :"+obj.sum(10,7));
+
+    }
+}
