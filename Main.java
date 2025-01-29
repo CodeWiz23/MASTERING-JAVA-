88027d1ca6e3548f817355b674995d224775cd08
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2025-01-29 22:20:13 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello world");
+
+        int num1 = 1;
+        int num2 = 5;
+        int num3 = 1;
+        int num4 = 1;
+        int sum = num1+num2+num3+num4;
+        System.out.println(sum);
+    }
+}
